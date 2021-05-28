9b54571c5f99b9a1e59b10328e13edd788266041
Matsumura Haruka <lumina_168@yahoo.co.jp>
2021-05-28 20:58:33 +0900

    HTML、DB出せるようなった

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import sqlite3
 app = Flask(__name__)
 
 @app.route('/')
@@ -14,9 +15,9 @@ def profile(username):
     return "ここは、"+username+"さんのプロフィールネームです"
 
 
-# @app.route('/temptest')
-# def temptest():
-#     return render_template('index.html')
+@app.route('/temptest')
+def temptest():
+    return render_template('index.html')
 
 @app.route('/extendtest')
 def extendtest():
@@ -24,11 +25,29 @@ def extendtest():
 
 
 @app.route('/weather')
-def temptest():
+def weather():
     # ↑これが被ったらエラー起こす。だから上をメッセージ用にして消した
-    title = '快晴〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜'
-    message = '今日の天気は〜〜〜〜〜〜〜〜'
-    return render_template('index.html', title=title, message=message)
+    weather = "快晴"
+    title = '今日の天気は'
+    message = '快晴'
+    return render_template('index.html', title=title, message=message, weather=weather)
+
+
+
+@app.route('/dbtest')
+def dbtest():
+    # dbファイルに接続
+    conn = sqlite3.connect("flasktest.db")
+    c = conn.cursor()
+    # SQL分でデータを取り出し
+    c.execute("""select name, age, address from users where id= 1""")
+    user_info = c.fetchone()
+    # dbファイルとの接続を終了
+    c.close()
+    # 取り出したデータの中身を確認
+    print(user_info)
+    return render_template('db.html', user_info=user_info)
+    # エラー分はしっかり見る（エラー分が出てくるから）
 
 if __name__ == "__main__":
     app.run(debug=True)
